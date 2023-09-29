48909794c91909be32301dbe590f5696c3e07a5e
skifli <121291719+skifli@users.noreply.github.com>
2023-09-29 16:21:57 +0000

    feat: push part 1 oneliner and cleanup

---
+++ 2023-24/QOTW #1/intermediate/part_1.py
@@ -0,0 +1,25 @@
+"""
+One-liner in `part_1-oneliner.py`.
+"""
+
+from os.path import split
+
+path = split(__file__)[0]  # If terminal cwd is different to program path.
+
+with open(f"{path}/assets/email.txt", "r", encoding="utf-8") as f:
+    lines = [line.rstrip("\n") for line in f][2:]
+
+for line in lines:
+    chars, to_find = line.split(" ")
+    char_1, char_2, char_3 = to_find
+    char_1_count, char_2_count, char_3_count = [0]*3
+
+    for char in chars:
+        if char == char_1:
+            char_1_count += 1
+        elif char == char_2:
+            char_2_count += 1
+        elif char == char_3:
+            char_3_count += 1
+
+    print(f"{char_1_count}{char_2_count}{char_3_count}")
